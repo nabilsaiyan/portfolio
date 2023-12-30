c626f19048d143cc88527da85303179499571ced
Nabilsenko <96882497+Nabilsenko@users.noreply.github.com>
2023-12-30 21:31:53 +0100

    feat(ui): Add responsiveness to the 3d model in intro section

--- src/components/Canvas/AvatarCanvas.tsx
+++ src/components/Canvas/AvatarCanvas.tsx
@@ -1,15 +1,47 @@
 import { Canvas } from '@react-three/fiber'
-import { Suspense } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { Environment } from '@react-three/drei'
 import Setup from '../Avatars/Setup'
+import { Vector3 } from 'three'
 
 function AvatarCanvas() {
+  const [cameraPosition, setCameraPosition] = useState<Vector3>(
+    new Vector3(-3, 3.8, -17),
+  )
+  const [setupPosition, setSetupPosition] = useState<Vector3>(
+    new Vector3(-5, 2, 0),
+  )
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth
+      if (screenWidth < 1000) {
+        if (screenWidth < 450) {
+          setCameraPosition(new Vector3(2, 3.8, -17))
+          setSetupPosition(new Vector3(0, 2, 0)) // Change setup position for width < 450px
+        } else {
+          setCameraPosition(new Vector3(0, 3.8, -17))
+          setSetupPosition(new Vector3(-2, 2, 0)) // Change setup position for width < 768px
+        }
+      } else {
+        setCameraPosition(new Vector3(-3, 3.8, -17))
+        setSetupPosition(new Vector3(-5, 2, 0)) // Default setup position for larger screens
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return (
-    <Canvas shadows camera={{ position: [-3, 3.8, -17], fov: 60 }}>
+    <Canvas shadows camera={{ position: cameraPosition, fov: 60 }}>
       <ambientLight />
       <Environment preset="sunset" />
       <Suspense fallback={null}>
-        <Setup position={[-5, 2, 0]} rotation={[-0.4, -1, 0]} scale={0.7} />
+        <Setup position={setupPosition} rotation={[-0.4, -1, 0]} scale={0.7} />
       </Suspense>
     </Canvas>
   )
